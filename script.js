5307ed5ebee7d4063531d977b81af85b000d4d20
Kenson Johnson <kenson.johnson@outlook.com>
2023-02-14 21:09:17 -0600

    Removed menu toggling

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ imgBx.forEach((image) =>
   })
 );
 
-function toggleMenu() {
-  var menuToggle = document.querySelector("[data-menu-button]");
-  var navigation = document.querySelector(".navigation");
-  menuToggle.classList.toggle("active");
-  navigation.classList.toggle("active");
-}
+// function toggleMenu() {
+//   var menuToggle = document.querySelector("[data-menu-button]");
+//   var navigation = document.querySelector(".navigation");
+//   menuToggle.classList.toggle("active");
+//   navigation.classList.toggle("active");
+// }
