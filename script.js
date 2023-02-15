3aafb1929a6436232c212fb333d01a08c7c68089
Kenson Johnson <kenson.johnson@outlook.com>
2023-02-14 20:31:55 -0600

    Extracted JS to script file

---
+++ script.js
@@ -0,0 +1,14 @@
+const imgBx = document.querySelectorAll(".imgBx");
+
+imgBx.forEach((image) =>
+  image.addEventListener("click", () => {
+    image.classList.toggle("active");
+  })
+);
+
+function toggleMenu() {
+  var menuToggle = document.querySelector("[data-menu-button]");
+  var navigation = document.querySelector(".navigation");
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+}
