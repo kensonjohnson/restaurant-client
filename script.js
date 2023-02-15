876fb305cdbf4ad47e5516c3d2ecc27b729c76a7
Kenson Johnson <kenson.johnson@outlook.com>
2023-02-14 21:36:36 -0600

    Cleanup html and css files.

--- script.js
+++ script.js
@@ -5,10 +5,3 @@ imgBx.forEach((image) =>
     image.classList.toggle("active");
   })
 );
-
-// function toggleMenu() {
-//   var menuToggle = document.querySelector("[data-menu-button]");
-//   var navigation = document.querySelector(".navigation");
-//   menuToggle.classList.toggle("active");
-//   navigation.classList.toggle("active");
-// }
